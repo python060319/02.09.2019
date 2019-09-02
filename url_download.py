a2400b97016b3a86fc3b174e2029abdedc18223a
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-02 19:25:39 +0300

    Create url_download.py

---
+++ url_download.py
@@ -0,0 +1,5 @@
+import urllib.request
+import threading
+import time
+
+urllib.request.urlretrieve('https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/152964589-welcome-home-new-cat-632x475.jpg', 'c:/itay/cat.jpg')

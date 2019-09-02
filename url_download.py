725fd48270875596bae88da24ec06958317de9c3
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-02 19:36:53 +0300

    Update url_download.py

--- url_download.py
+++ url_download.py
@@ -2,7 +2,16 @@ import urllib.request
 import threading
 import time
 import os
-#urllib.request.urlretrieve('http://www.study-io.com/java/installations/mongodb-win32-x86_64-2008plus-ssl-3.6.3-signed.msi','c:/itay/1.msi')
+
 url = 'https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/152964589-welcome-home-new-cat-632x475.jpg'
 filename = os.path.basename(url)
 urllib.request.urlretrieve(url, f'c:/itay/{filename}')
+
+# one thread dlownload thi huge file
+# 'http://www.study-io.com/java/installations/mongodb-win32-x86_64-2008plus-ssl-3.6.3-signed.msi'
+# second thread counts the seconds...
+# when download completed counting thread will finish
+# *etgar: show percentage
+# urlopen -> info
+# os - what is thc urrent file size
+# 100 * current / total = %

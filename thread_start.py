de6b1bf4c50ec1cbd068c16e2243019267ff213b
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-02 18:02:04 +0300

    Update thread_start.py

--- thread_start.py
+++ thread_start.py
@@ -13,3 +13,7 @@ t1.start() # new thread call sleeper
 print("\nmain has left the building...")
 #t1.join()
 # cannot go home...
+
+# targil
+# main - input a number
+# create a thread (foreground) which counts from 5 to 0 in 1 second interval

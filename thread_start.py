91d5a2d1abcc947f6840f815ab303588e884cabd
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-02 18:54:16 +0300

    Update thread_start.py

--- thread_start.py
+++ thread_start.py
@@ -1,6 +1,7 @@
 import threading
 import time
 
+pleaseStop = False
 def sleeper():
     print(threading.currentThread().name)
     print("start")
@@ -19,18 +20,27 @@ def sleeper():
 # cannot go home...
 
 def countDown(number):
+    global pleaseStop
     while True:
         number = number - 1
         print(number)
         time.sleep(1)
-        if number == 0:
+        if number == 0 or pleaseStop:
             break
 
 t1 = threading.Thread(target=countDown
                       , args=(5,))
+#t1.daemon = True
+t1.start()
+
+t1 = threading.Thread(target=countDown
+                      , args=(5,))
+#t1.daemon = True
 t1.start()
 x = input("\n5 * 12 = ?")
+
 if t1.isAlive():
     print("On time!")
+    pleaseStop = True
 else:
     print("TimeOut!")

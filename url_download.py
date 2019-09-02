bb8fd2293db3bd7522758695706a37616c1bb3ea
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-02 19:34:14 +0300

    Update url_download.py

--- url_download.py
+++ url_download.py
@@ -1,8 +1,8 @@
 import urllib.request
 import threading
 import time
-
+import os
 #urllib.request.urlretrieve('http://www.study-io.com/java/installations/mongodb-win32-x86_64-2008plus-ssl-3.6.3-signed.msi','c:/itay/1.msi')
 url = 'https://d17fnq9dkz9hgj.cloudfront.net/uploads/2012/11/152964589-welcome-home-new-cat-632x475.jpg'
-#filename = ?
+filename = os.path.basename(url)
 urllib.request.urlretrieve(url, f'c:/itay/{filename}')

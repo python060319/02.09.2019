f6b7f0d865aac95f94c75ad6971541d8e1f2b0db
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-02 18:36:23 +0300

    Update thread_start.py

--- thread_start.py
+++ thread_start.py
@@ -2,18 +2,35 @@ import threading
 import time
 
 def sleeper():
+    print(threading.currentThread().name)
     print("start")
     time.sleep(4)
     print('end')
 
-t1 = threading.Thread(target=sleeper)
-t1.daemon = False # Background
-t1.start() # new thread call sleeper
+#t1 = threading.Thread(target=sleeper
+#      ,name="my Thread")
+#print(t1.name)
+
+#print(threading.currentThread().name)
+#t1.daemon = True # Background
+#t1.start() # new thread call sleeper
 #sleeper() # main call sleeper
-print("\nmain has left the building...")
-#t1.join()
+#print("\nmain has left the building...")
 # cannot go home...
 
-# targil
-# main - input a number
-# create a thread (foreground) which counts from 5 to 0 in 1 second interval
+def countDown(number):
+    while True:
+        number = number - 1
+        print(number)
+        time.sleep(1)
+        if number == 0:
+            break
+
+t1 = threading.Thread(target=countDown
+                      , args=(5,))
+t1.start()
+x = input("\n5 * 12 = ?")
+if t1.isAlive():
+    print("On time!")
+else:
+    print("TimeOut!")

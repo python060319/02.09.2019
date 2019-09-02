270e9d8a89eca64c3ea8bafe3fa5f96260d11e39
python060319 <51337287+python060319@users.noreply.github.com>
2019-09-02 20:27:08 +0300

    Update url_download.py

--- url_download.py
+++ url_download.py
@@ -16,5 +16,29 @@ urllib.request.urlretrieve(url, f'c:/itay/{filename}')
 # os - what is thc urrent file size
 # 100 * current / total = %
 
-d = urllib.request.urlopen('http://www.study-io.com/java/installations/mongodb-win32-x86_64-2008plus-ssl-3.6.3-signed.msi')
-print(d.info().get('content-length'))
+# Solution:
+def download():
+    global filename
+    urllib.request.urlretrieve(url, localFile)
+
+def counting():
+    global filename
+    last = -1
+    while t1.isAlive():
+        time.sleep(1)
+        curSize = os.stat(localFile).st_size
+        if not curSize == last: # print only if there was a progress
+            last = curSize
+            print(f'%{curSize * 100 // maxsize}')
+
+
+url = 'http://www.study-io.com/java/installations/mongodb-win32-x86_64-2008plus-ssl-3.6.3-signed.msi'
+filename = os.path.basename(url)
+localFile = f'c:/itay/{filename}'
+d = urllib.request.urlopen(url)
+maxsize = int(d.info().get('content-length'))
+t1 = threading.Thread(target=download)
+t2 = threading.Thread(target=counting)
+t1.start()
+t2.start()
+
